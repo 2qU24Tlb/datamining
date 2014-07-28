f1e1ec70b34be6ae1764d7e6dda814fc49dc1d9a
unioah <unioah@gmail.com>
2014-07-28 13:01:39 -0500

    init tire-based-paroori

---
+++ others/tire-baesd-apriori-motif/main.cc
@@ -0,0 +1,9 @@
+#include "tire.h"
+#include <iostream>
+
+using namespace std;
+
+int main (void) {
+
+  return 0;
+}
---
+++ others/tire-baesd-apriori-motif/tire.h
@@ -0,0 +1,12 @@
+#ifndef TIRE_BASED_APRIORI_MOTIF
+#define TIRE_BASED_APRIORI_MOTIF
+
+using namespace std;
+
+class windows_tire {
+ private:
+
+ public:
+};
+
+#endif
