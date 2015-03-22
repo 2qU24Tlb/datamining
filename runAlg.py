8219562b37904fa112f824941d883325d25c8359
unioah <unioah@gmail.com>
2015-03-22 13:50:10 -0500

    add spark

--- runAlg.py
+++ runAlg.py
@@ -1,34 +1,50 @@
 #!/bin/python
+# universal script for running different program
 
 import os
 import sys
 from subprocess import call
 
-#java -jar spmf.jar run Apriori A1.txt output.txt 70%
-
 algList = ["Apriori"]
-
 alg = ""
 db = ""
 minsup = 0.5
 
-if (len(sys.argv) == 5):
+if (len(sys.argv) == 2):
+    if (sys.argv[1] == "list"):
+        print(algList)
+    elif (sys.argv[1] == "example"):
+        print("./runAlg.py py Apriori DB/A1.txt 0.5")
+
+elif (len(sys.argv) == 5):
     alg = sys.argv[2]
     db = sys.argv[3]
     minsup = sys.argv[4]
 
-    if (sys.argv[1] == "my"): 
-        print(["myAlg_PY/" + alg + ".py", db, minsup])
-        call(["myAlg_PY/" + alg + ".py", db, minsup])
-    elif(sys.argv[1] == "spmf"):
-        #print(["java", "-jar", "spmf/spmf.jar", \
-        #       "run", alg, db, "output", minsup])
-        call(["java", "-jar", "spmf/spmf.jar", \
-              "run", alg, db, "output", minsup])
+    # run python program
+    if (sys.argv[1] == "py"):
+        command = ["myAlg_PY/" + alg + ".py", db, minsup]
+        print(command)
+        call(command)
 
-elif (len(sys.argv) == 2 and sys.argv[1] == "list"):
-    print(algList)
-elif (len(sys.argv) == 2 and sys.argv[1] == "exam"):
-    print("./runAlg.py my Apriori DB/A1.txt 0.5")
+    # run spmf program
+    elif(sys.argv[1] == "spmf"):
+        # java -jar spmf.jar run Apriori A1.txt output.txt 70%
+        command = ["java", "-jar", "spmf/spmf.jar",
+                   "run", alg, db, "output", minsup]
+        print(command)
+        call(command)
+
+    # run spark program
+    elif(sys.argv[1] == "spark"):
+        server = "spark://myArch:7077"
+        command = ["/usr/local/spark/bin/spark-submit",
+                   "--class", "org.spark." + alg.lower() + "." + alg,
+                   "--master", server,
+                   "myAlg_spark/targets/" + alg + "-0.1.jar",
+                   db, minsup]
+        print(command)
+        call(command)
+
 else:
-    print("runAlg.py", "algorithm", "DB", "minsup")
+    print("usage: runAlg.py", "type", "algorithm", "DB", "minsup")
