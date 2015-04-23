93a3e587a0cbcdb4313b89edc3aec86d3bc237c6
unioah <unioah@gmail.com>
2015-04-23 12:52:54 -0500

    update

--- myAlg_spark/SVT/src/main/scala/myAlg/spark/svt/SVTDriver.scala
+++ myAlg_spark/SVT/src/main/scala/myAlg/spark/svt/SVTDriver.scala
@@ -2,8 +2,8 @@ package myAlg.spark.svt
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.SparkContext._
+import org.apache.spark.rdd.RDD._
 import org.apache.spark.RangePartitioner
-import myAlg.spark.svt._
 
 
 object SVT {
@@ -11,19 +11,13 @@ object SVT {
 
     val sparkConf = new SparkConf().setAppName("Scale Vertical Mining")
     val sc = new SparkContext(sparkConf)
-    val file = sc.textFile ("hdfs:///data/A1.txt")
+    val file = sc.textFile ("/tmp/A1.txt")
     val minSup = 0.5
     val fileSize = file.count
-    //var results: String = Nil
+    var results = List()
 
     // stage 1: obtain global frequent list, col 1 is transaction id
-    val FItemsL1 = file.flatMap(line =>
-      line.split(" ")
-        .drop(1)
-        .map(item=>(item, 1)))
-      .reduceByKey(_ + _)
-      .filter(_._2 >= minSup * fileSize)
-      .collectAsMap
+    val FItemsL1 = file.flatMap(line => line.split(" ") .drop(1) .map(item=>(item, 1))) .reduceByKey(_ + _) .filter(_._2 >= minSup * fileSize) .collectAsMap
     // method 2---> .reduceByKey(_ + _).filter(_._2 >= minSup * fileSize).sortBy(_._1).cache
 
     // method1: broadcast 1 level support list
@@ -32,47 +26,52 @@ object SVT {
     // use rdd operation lookup() instead
 
     // stage 2: generate frequent 2 item sets
-    val candidates = file.flatMap(line =>
-      (line.split(" ")
-        .tail
-        .filter(BTVal.value.contains(_))
-        .sorted
-        .combinations(2)
-        .map(i => (i(0) + i(1), line(0).toString))))
-      .reduceByKey((x, y) => (x+y).distinct.sorted).cache
+    val candidates = file.flatMap(line => (line.split(" ") .tail .filter(BTVal.value.contains(_)) .sorted .combinations(2) .map(i => (i.toList, List(line(0).asDigit))))) .reduceByKey((x, y) => (x ++ y).distinct.sorted).cache
 
     // stage 3: re-partition & local vertical mining
 // [FixMe] Calculate the size of equivalent class
-    val newGroup = candidates.keyBy(key => key._1.head)
-    val GroupedCands = newGroup.partitionBy(new RangePartitioner[Char, (String,String)]
-      (BTVal.value.size, newGroup)).values
-    //or we should use repartitionAndSortWithinPartitions ?
+    val newGroup = candidates.keyBy(_._1(0))
+//or we should use repartitionAndSortWithinPartitions ?
+    val GroupedCands = newGroup.partitionBy(new RangePartitioner[String, (List[String], List[Int])] (BTVal.value.size, newGroup)).values
 
     // we can check the partition status by using: 
-    // LCands.mapPartitionsWithIndex((idx, itr) => itr.map(s => (idx, s))).collect.foreach(println)
+    // GroupedCands.mapPartitionsWithIndex((idx, itr) => itr.map(s => (idx, s))).collect.foreach(println)
 
-    var counts = GroupedCands.mapPartitions(genCandidates, preservesPartitioning = true)
+    var EQClass = GroupedCands.mapPartitions(genCandidates, preservesPartitioning = true).filter(_._2.length >= minSup * fileSize).cache
 
-    // keep generating the frequent items
-    // while (counts.size != 1) {
-      //counts = GroupedCands.mapPartitions(genCandidates, preservesPartitioning = true)
-    //}
-    counts.collect
+// [FixMe] try declat
+    while (EQClass.count > 1) {
+      EQClass = EQClass.map(key => (key._1.take(key._1.length - 1), List((key._1.last, key._2)))).reduceByKey(_ ++ _).filter(_._2.length >= 2).flatMap(x => eclat(x._1, x._2, (minSup * fileSize).toInt))
+        .filter(x => x._2.length  >= minSup * fileSize)
+    }
+
+    val results = EQClass.collect
+    println(results)
 
     //counts.saveAsTextFile("hdfs:///output/results")
 
     sc.stop()
   }
 
-  def genCandidates(iter: Iterator[(String, String)]) : Iterator[(String, String)] = {
+//[FiXMe] Can we just use Array instead of Iterator? 
+  def genCandidates(iter: Iterator[(List[String], List[Int])]) : Iterator[(List[String], List[Int])] = {
     val LList = iter.toArray
-    val LLength = LList.length 
     var i, j = 0
-
-    val result = for (i <- 0 to LLength; j <- i + 1 to LLength) yield
-      (((LList(i)_1)+(LList(i)_1)), ((LList(i)_2)+(LList(i)_2)))
+    val result = for (i <- 0 to LList.length - 1; j <- i + 1 to LList.length - 1) yield
+      (((LList(i)_1)++(LList(j)_1)).distinct.sorted, (LList(i)_2).intersect(LList(j)_2).distinct.sorted)
 
     result.iterator
+  }
+
+  //def eclat(key: List[String], keyValue: List[(String, List[Int])], minSup: Int): List[(List[String], List[Int])] = {
+  def eclat(key: List[String], value: List[(String, List[Int])], minSup: Int): List[(List[String], List[Int])] = {
+    var i, j = 0
+    val result = for (i <- 0 to value.length - 1; j <- i + 1 to value.length - 1) yield
+      ((key ++ List(value(i)._1) ++ List(value(j)._1)).sorted, (value(i)._2.intersect(value(j)._2)).distinct.sorted)
+    result.toList
+  }
+
+  def declat() {
   }
 
 }
--- myAlg_spark/run.sh
+++ myAlg_spark/run.sh
@@ -3,13 +3,14 @@
 app=$2
 rcmd="/usr/local/spark/bin/spark-submit "
 pcmd="mvn package"
-input="hdfs:///data/A1.txt"
-output="hdfs:///output/results"
+#input="hdfs:///data/A1.txt"
+#output="hdfs:///output/results"
 
 if [ $1 == "run" ]; then
     echo "run:"
-    /usr/local/hadoop/bin/hadoop fs -rm -r /output/*
-    ${rcmd} --class myAlg.spark.${app,,}.${app} --master yarn ${app}/target/${app}-0.1.jar ${input} ${output}
+    #/usr/local/hadoop/bin/hadoop fs -rm -r /output/*
+    #${rcmd} --class myAlg.spark.${app,,}.${app} --master yarn ${app}/target/${app}-0.1.jar ${input} ${output}
+    ${rcmd} --class myAlg.spark.${app,,}.${app} --master spark://myArch:7077 ${app}/target/${app}-0.1.jar ${input} ${output}
 elif [ $1 == "com" ]; then
     echo "compile:"
     cd ${app}
