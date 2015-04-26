f40c7283ddb0e09e60859dfa2571502e4427b4de
unioah <unioah@gmail.com>
2015-04-25 22:22:19 -0500

    update

--- myAlg_spark/SVT/src/main/scala/myAlg/spark/svt/SVTDriver.scala
+++ myAlg_spark/SVT/src/main/scala/myAlg/spark/svt/SVTDriver.scala
@@ -14,9 +14,15 @@ object SVT {
     val file = sc.textFile (args(0))
     val minSup = args(1).toDouble
     val fileSize = file.count
+    //var results = "hdfs:///output/result"
 
     // stage 1: obtain global frequent list, col 1 is transaction id
-    val FItemsL1 = file.flatMap(line => line.split(" ") .drop(1) .map(item=>(item, 1))) .reduceByKey(_ + _) .filter(_._2 >= minSup * fileSize) .collectAsMap
+    val FItemsL1 = file.flatMap(line => line.split(" ")
+      .drop(1)
+      .map(item=>(item, 1)))
+      .reduceByKey(_ + _)
+      .filter(_._2 >= minSup * fileSize)
+      .collectAsMap
     // method 2---> .reduceByKey(_ + _).filter(_._2 >= minSup * fileSize).sortBy(_._1).cache
 
     // method1: broadcast 1 level support list
@@ -25,45 +31,60 @@ object SVT {
     // use rdd operation lookup() instead
 
     // stage 2: generate frequent 2 item sets
-    val candidates = file.flatMap(line => (line.split(" ") .tail .filter(BTVal.value.contains(_)) .sorted .combinations(2) .map(i => (i.toList, List(line(0).asDigit))))) .reduceByKey((x, y) => (x ++ y).distinct.sorted).cache
+    val candidates = file.flatMap(line => (line.split(" ")
+      .tail
+      .filter(BTVal.value.contains(_))
+      .sorted
+      .combinations(2)
+      .map(i => (i.toList, List(line(0).asDigit)))))
+      .reduceByKey((x, y) => (x ++ y).distinct.sorted)
 
     // stage 3: re-partition & local vertical mining
-// [FixMe] Calculate the size of equivalent class
+// [FixMe] Calculate the size of equivalent class to make sure that fit the memory
     val newGroup = candidates.keyBy(_._1(0))
 //or we should use repartitionAndSortWithinPartitions ?
-    val GroupedCands = newGroup.partitionBy(new RangePartitioner[String, (List[String], List[Int])] (BTVal.value.size, newGroup)).values
-
+    val GroupedCands = newGroup.
+      partitionBy(new RangePartitioner[String, (List[String], List[Int])]
+        (BTVal.value.size, newGroup)).values
     // we can check the partition status by using: 
-    // GroupedCands.mapPartitionsWithIndex((idx, itr) => itr.map(s => (idx, s))).collect.foreach(println)
+    //GroupedCands.mapPartitionsWithIndex((idx, itr) => itr.map(s => (idx, s))).collect.foreach(println)
 
-    var EQClass = GroupedCands.mapPartitions(genCandidates, preservesPartitioning = true).filter(_._2.length >= minSup * fileSize).cache
+    var EQClass = GroupedCands.
+      //mapPartitions(genCandidates, preservesPartitioning = true)
+      mapPartitions(genCandidates)
+      .filter(_._2.length >= minSup * fileSize).cache
 
 // [FixMe] try declat
     while (EQClass.count > 1) {
-      EQClass = EQClass.map(key => (key._1.take(key._1.length - 1), List((key._1.last, key._2)))).reduceByKey(_ ++ _).filter(_._2.length >= 2).flatMap(x => eclat(x._1, x._2, (minSup * fileSize).toInt))
-        .filter(x => x._2.length  >= minSup * fileSize)
-      EQClars.saveAsTextFile("hdfs:///output/results")
+      EQClass = EQClass.map(key => (key._1.take(key._1.length - 1), List((key._1.last, key._2))))
+        .reduceByKey(_ ++ _)
+        .filter(_._2.length >= 2)
+        .flatMap(x => eclat(x._1, x._2, (minSup * fileSize).toInt))
+        .filter(x => x._2.length  >= minSup * fileSize).cache
+      //EQClars.saveAsTextFile(results)
     }
-
 
     sc.stop()
   }
 
 //[FiXMe] Can we just use Array instead of Iterator? 
-  def genCandidates(iter: Iterator[(List[String], List[Int])]) : Iterator[(List[String], List[Int])] = {
+  def genCandidates(iter: Iterator[(List[String], List[Int])]) :
+      Iterator[(List[String], List[Int])] = {
     val LList = iter.toArray
     var i, j = 0
     val result = for (i <- 0 to LList.length - 1; j <- i + 1 to LList.length - 1) yield
-      (((LList(i)_1)++(LList(j)_1)).distinct.sorted, (LList(i)_2).intersect(LList(j)_2).distinct.sorted)
+      (((LList(i)_1)++(LList(j)_1)).distinct.sorted,
+        (LList(i)_2).intersect(LList(j)_2).distinct.sorted)
 
     result.iterator
   }
 
-  //def eclat(key: List[String], keyValue: List[(String, List[Int])], minSup: Int): List[(List[String], List[Int])] = {
-  def eclat(key: List[String], value: List[(String, List[Int])], minSup: Int): List[(List[String], List[Int])] = {
+  def eclat(key: List[String], value: List[(String, List[Int])], minSup: Int) :
+      List[(List[String], List[Int])] = {
     var i, j = 0
     val result = for (i <- 0 to value.length - 1; j <- i + 1 to value.length - 1) yield
-      ((key ++ List(value(i)._1) ++ List(value(j)._1)).sorted, (value(i)._2.intersect(value(j)._2)).distinct.sorted)
+      ((key ++ List(value(i)._1) ++ List(value(j)._1)).sorted,
+        (value(i)._2.intersect(value(j)._2)).distinct.sorted)
     result.toList
   }
 
