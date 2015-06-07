fe6871d2c8c6ab4cef5b2059df8209d7a633d515
unioah <unioah@gmail.com>
2015-06-06 21:43:13 -0500

    update

--- myAlg_spark/run.sh
+++ myAlg_spark/run.sh
@@ -9,10 +9,10 @@ MINSUP="0.1"
 CONF="--executor-memory 10G --conf spark.eventLog.enabled=true"
 
 if [ $1 == "run" ]; then
-    ${RCMD} --class myAlg.spark.${APP,,}.MyTest --master ${ADDRESS} ${CONF} ${APP}/target/${APP}-0.1.jar ${DB1} ${MINSUP} 
+    ${RCMD} --class myAlg.spark.${APP,,}.MyTest --master ${ADDRESS} ${CONF} ${APP}/target/${APP}-0.1.jar ${DB2} ${MINSUP} 
 elif [ $1 == "com" ]; then
     cd ${APP}
     mvn clean -q && mvn package -q
 elif [ $1 == "exam" ]; then
-    ${RCMD} --class org.apache.spark.examples.mllib.FPGrowthExample --master ${ADDRESS}  ${CONF} /usr/local/spark/lib/spark-examples-1.3.1-hadoop2.6.0.jar --minSupport ${MINSUP} --numPartition 4 ${DB2}
+    ${RCMD} --class org.apache.spark.examples.mllib.FPGrowthExample --master ${ADDRESS}  ${CONF} /usr/local/spark/lib/spark-examples-1.3.1-hadoop2.6.0.jar --minSupport ${MINSUP} --numPartition 4 ${DB1}
 fi
