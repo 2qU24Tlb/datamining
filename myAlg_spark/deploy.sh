bf46f9e8944113d7382d8ab013c7ec65481c6bf9
unioah <unioah@gmail.com>
2016-08-09 16:39:17 -0500

    update

--- myAlg_spark/deploy.sh
+++ myAlg_spark/deploy.sh
@@ -73,10 +73,10 @@ function run {
                --master ${MASTER} ${CONF} \
                $PWD/Peclat/target/scala-2.10/peclat_2.10-1.0.jar \
                ${MINSUP}
-    elif [ $APP == "YAFIM" ]; then
+    elif [ $APP == "yafim" ]; then
         ${CMD} --class YAFIMtest \
                --master ${MASTER} ${CONF} \
-               $PWD/YAFIM/target/scala-2.10/yafim_2.10-3.0.jar \
+               $PWD/YAFIM/target/scala-2.10/yafim_2.10-1.0.jar \
                ${MINSUP}
     fi
 }
